43cda49c730a3ffa0c7554664acdc21f3066525e
think101 <taohuayuan2000@hotmail.com>
2022-09-03 22:48:21 -0700

    format files

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,17 +1,20 @@
 import wikipedia
 from textblob import TextBlob
 
+
 def wiki(name="War Goddess", length=1):
     """This is a wikipedia fetcher"""
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
 
+
 def search_wiki(name):
     """Search wikipedia for name"""
 
     my_wiki = wikipedia.search(name)
     return my_wiki
+
 
 def phrase(name):
     """This is a phrase analyzer for wikipedia page"""
--- test_main.py
+++ test_main.py
@@ -4,19 +4,21 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Wikipedia API. Call /search or /wiki"}
+
 
 def test_phrase():
     response = client.get("/phrase/Barack Obama")
     assert response.status_code == 200
     assert response.json() == {
         "result": [
-    "barack hussein obama ii",
-    "bə-rahk hoo-sayn oh-bah-mə",
-    "august",
-    "american politician",
-    "44th president"
-  ]}
+            "barack hussein obama ii",
+            "bə-rahk hoo-sayn oh-bah-mə",
+            "august",
+            "american politician",
+            "44th president"
+        ]}
